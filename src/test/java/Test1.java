bf28198a6df4509498d2e67a4d347b4fc764d5ac
OpereiraG <npereira1633@gmail.com>
2023-09-07 21:25:39 -0400

    HouseProjectV1

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,2 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class Test1 {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://www.homedepot.com/");
+        String correctTitle = "The Home Depot";
+        String currentTitle = driver.getTitle();
+        //System.out.println(driver.getTitle());
+        Assert.assertEquals(correctTitle, currentTitle, "Title does not match the requirements");
+
+
+    }
 }
